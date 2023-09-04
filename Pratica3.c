f1df14e9863980a5c5872ae27e3c5dbaf4b3a523
TalisonMoura <talisonmoura13@gmail.com>
2023-09-03 21:01:38 -0300

    Questão 3 Ponteiro

---
+++ Pratica3.c
@@ -0,0 +1,20 @@
+/*
+Faça um programa na Linguagem C usando ponteiro, que receba dois valores
+inteiros dentro de uma função e realize a soma. A função poderá ser do tipo do
+tipo void, assim, a resposta é retornada pelo parâmetro resultado, que é um
+ponteiro.
+
+*/
+
+void soma(int a, int b, int *c) {
+	*r = val1 + val2;
+}
+
+int main() {
+	int a = 10,b = 20;
+	int result;
+	
+	soma(a,b,&result);
+	printf("Resultado: %d", result);
+return(0);
+}
